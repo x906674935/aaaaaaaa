91f19f509062b57186fd604ca47b75bcbd1fd083
xie <xieyf0326.sina.com>
2019-06-27 21:18:40 +0800

    项目开始

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+//vuex一律使用模块
+export default new Vuex.Store({
+  state: {                
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  }
+})
